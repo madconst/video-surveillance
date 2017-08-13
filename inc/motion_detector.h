f0ae4ddb1a58a08cfe922a17c9a14e59afa0b3e1
Constantine Yemelyanov <kostya.emelyanov@gmail.com>
2017-08-13 22:48:10 +0300

    Added threshold in seconds as motion detector constructor parameter.

--- inc/motion_detector.h
+++ inc/motion_detector.h
@@ -4,14 +4,18 @@
 #include <chrono>
 #include "motion_estimator.h"
 
+using namespace std::chrono_literals;
+
 class MotionDetector
 {
 public:
+  explicit MotionDetector(std::chrono::seconds threshold = 3s);
   bool operator()(const cv::Mat& frame);
   cv::Mat diff();
 private:
   MotionEstimator estimator_;
   std::chrono::steady_clock::time_point last_detected_;
+  const std::chrono::seconds threshold_;
 };
 
 #endif // MOTION_DETECTOR_H
--- src/motion_detector.cpp
+++ src/motion_detector.cpp
@@ -1,13 +1,15 @@
 #include "inc/motion_detector.h"
 
+MotionDetector::MotionDetector(std::chrono::seconds threshold) :
+  threshold_(threshold)
+{}
 bool MotionDetector::operator()(const cv::Mat& frame)
 {
-  using namespace std::chrono_literals;
   double diff = estimator_(frame);
   if (diff > 2.0) {
     last_detected_ = std::chrono::steady_clock::now();
   }
-  return std::chrono::steady_clock::now() - last_detected_ < 1s;
+  return std::chrono::steady_clock::now() - last_detected_ < threshold_;
 }
 cv::Mat MotionDetector::diff()
 {
