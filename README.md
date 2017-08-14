3bcc4c16b329794aae5624f02735d250c0bea2a1
Constantine Yemelyanov <kostya.emelyanov@gmail.com>
2017-08-14 22:29:28 +0300

    Added Boost program options and README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# Build
+
+In the source folder run:
+```
+mkdir build
+cd build
+cmake ..
+make -j 4
+```
+
+## Dependencies
+
+The project depends on the following libraries:
+
+ * OpenCV 3
+ * FFmpeg
+ * Boost Program Options
+
+# Run
+
+```
+$ mkdir out
+$ ./app/netcam -i rtsp://some.cam/url -o out/
+2017-08-14 22:22:43 Opened rtsp://some.cam/url
+2017-08-14 22:22:49 Started recording out//20170814_222249_1.mp4
+2017-08-14 22:22:49 Saved image out//20170814_222249_1_1.jpg
+2017-08-14 22:22:51 Saved image out//20170814_222251_1_2.jpg
+2017-08-14 22:22:53 Saved image out//20170814_222253_1_3.jpg
+2017-08-14 22:22:56 Saved image out//20170814_222256_1_4.jpg
+2017-08-14 22:22:57 Saved image out//20170814_222257_1_5.jpg
+2017-08-14 22:23:00 Stopped recording
+```
--- app/netcam.cpp
+++ app/netcam.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <fstream>
 #include <stdio.h>
+#include <boost/program_options.hpp>
 extern "C" {
   #include <libavformat/avio.h>
   #include <libavdevice/avdevice.h>
@@ -18,10 +19,13 @@ extern "C" {
 #include "inc/recorder.h"
 #include "inc/encoder.h"
 
-const std::string url = "rtsp://some.cam/url";
-const std::string out_directory = "out";
-const std::string file_prefix = "motion_";
-const std::string video_format = "ts";
+struct Config
+{
+  std::string input;
+  std::string output_directory;
+  std::string file_prefix;
+  std::string video_format = "mp4";
+} config;
 
 bool stop = false;
 
@@ -73,23 +77,68 @@ struct Statistics
   size_t chunks;
 };
 
-int main()
+void init_ffmpeg()
 {
   avdevice_register_all();
   avcodec_register_all();
   av_register_all();
   avformat_network_init();
+}
+
+void process_options(int argc, const char* argv[])
+{
+  // using namespace boost::program_options;
+  namespace po = boost::program_options;
+
+  po::options_description desc{"Options"};
+  desc.add_options()
+  ("help,h", "This help message")
+  ("input,i", po::value<std::string>(), "Input video stream")
+  ("output,o", po::value<std::string>(), "Output directory")
+  ("prefix,p", po::value<std::string>(), "File prefix");
+
+  po::variables_map vmap;
+  po::store(parse_command_line(argc, argv, desc), vmap);
+  po::notify(vmap);
+
+  if (vmap.count("input")) {
+    config.input = vmap["input"].as<std::string>();
+  }
+  if (vmap.count("output")) {
+    config.output_directory = vmap["output"].as<std::string>();
+  }
+  if (vmap.count("prefix")) {
+    config.output_directory = vmap["prefix"].as<std::string>();
+  }
+  if (vmap.count("help") || config.input.empty() || config.output_directory.empty()) {
+    std::cout << desc << std::endl;
+    exit(0);
+  }
+}
+
+int main(int argc, const char* argv[])
+{
+  try {
+    process_options(argc, argv);
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    exit(-1);
+  }
+
+  init_ffmpeg();
 
   Demuxer demux;
   try {
     demux
     .set_option("rtsp_flags", "prefer_tcp")
     .set_option("rtsp_flags", "prefer_tcp")
-    .open(url);
+    .open(config.input);
   } catch (const std::exception& e) {
     std::cerr << e.what() << std::endl;
     exit(-1);
   }
+
+  std::cout << date_time() << " Opened " << config.input << std::endl;
 
   MotionDetector detect;
 
@@ -97,7 +146,7 @@ int main()
   Decoder decoder(demux.stream_parameters(video_stream_index));
 
   Recorder recorder;
-  for (size_t i = 0; i < demux.num_of_streams(); ++i) {
+  for (size_t i = 0; i < demux. num_of_streams(); ++i) {
     recorder.add_stream(demux.stream_parameters(i));
   }
 
@@ -143,32 +192,30 @@ int main()
     recorder.flush(); // empty recorder on every new key frame
     if (detect(av2cv(frame))) {
       // motion detected
-      std::cout << date_time() << " Motion detected" << std::endl;
 
       // save keyframe as a JPEG image
       ++stats.key_frames_motion;
-      std::string filename = out_directory + "/"
-        + file_prefix + date_time("%Y%m%d_%H%M%S") + "_"
+      std::string filename = config.output_directory + "/"
+        + config.file_prefix + date_time("%Y%m%d_%H%M%S") + "_"
         + std::to_string(stats.chunks) + "_"
         + std::to_string(stats.key_frames_motion) + ".jpg";
       std::cout << date_time() << " Saved image " << filename << std::endl;
       save_jpeg(frame, filename);
 
-
       // start recording video
       if (!recorder.recording()) {
         ++stats.chunks;
-        std::string filename = out_directory + "/"
-            + file_prefix + date_time("%Y%m%d_%H%M%S") + "_"
+        std::string filename = config.output_directory + "/"
+            + config.file_prefix + date_time("%Y%m%d_%H%M%S") + "_"
             + std::to_string(stats.chunks)
-            + "." + video_format;
+            + "." + config.video_format;
         recorder.start_recording(filename);
         std::cout << date_time() << " Started recording " << filename << std::endl;
       }
     } else {
       if (recorder.recording()) {
         recorder.stop_recording();
-        std::cout << date_time() << " Motion stopped" << std::endl;
+        std::cout << date_time() << " Stopped recording" << std::endl;
       }
     }
     recorder.push(std::move(packet));
